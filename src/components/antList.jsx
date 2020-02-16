f288cb5ee5c1829b2fffd4cd3a3960cff93dc4bc
zIDAedgen <sdkfz233@icloud.com>
2020-02-16 09:34:50 +0900

    53

--- src/components/antList.jsx
+++ src/components/antList.jsx
@@ -1,12 +1,15 @@
 import React, { Fragment } from 'react';
-
+import 'antd/dist/antd.css';
+import { Input } from 'antd';
 
 class AntList extends React.Component {
     render() {
         return(
             <Fragment>
-                antd
+                <Input placeholder="Input Value Here" />
             </Fragment>
         );
     }
 }
+
+export default AntList;
