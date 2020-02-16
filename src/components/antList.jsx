6e1029caf6271c15fa98e22b109d814d1134face
zIDAedgen <sdkfz233@icloud.com>
2020-02-16 09:50:20 +0900

    53

--- src/components/antList.jsx
+++ src/components/antList.jsx
@@ -1,13 +1,27 @@
 import React, { Fragment } from 'react';
 import 'antd/dist/antd.css';
-import { Input } from 'antd';
+import { Input, Button, List} from 'antd';
 
+
+const data = [100, 200];
 class AntList extends React.Component {
+
     render() {
         return(
-            <Fragment>
-                <Input placeholder="Input Value Here" />
-            </Fragment>
+
+            <div style={{marginTop : '10px', marginLeft : '15px'}}>
+                <Input placeholder="Input Value Here" style={{width : '300px', marginRight : '15px'}} />
+                <Button type="danger">Submit</Button>
+                <div>
+                <List
+                    style={{width : '300px'}}
+                    size="large"            
+                    bordered
+                    dataSource={data}
+                    renderItem={item => <List.Item>{item}</List.Item>}
+    />
+                </div>
+            </div>
         );
     }
 }
